2a70d429784d5177b1ec6f7ee58258a1fd2618d3
Glenn <post@gohaugen.com>
2024-03-29 23:27:27 +0100

    Added price adjustment based on government subsidies

--- HomeAssistant.Service/Services/DailyHourPriceService.cs
+++ HomeAssistant.Service/Services/DailyHourPriceService.cs
@@ -63,7 +63,7 @@ public class DailyHourPriceService : IDailyHourPriceService
 
     public async Task<IEnumerable<IDailyHourPrice>> GetDailyHourPricesByDateAsync(DateTime date)
     {
-        return await _dailyHourPriceRepository.GetDailyHourPricesByDate(date);
+        return AdjustPricesAccordingToGovernmentSubsidies(await _dailyHourPriceRepository.GetDailyHourPricesByDate(date));
     }
 
     public async Task<int> FetchAndStoreDailyHourPricesByDateAsync(DateTime date)
@@ -86,6 +86,46 @@ public class DailyHourPriceService : IDailyHourPriceService
     private async Task<IEnumerable<IDailyHourPrice>> GetDailyHourPricesFromHvaKosterStrommenByDate(DateTime date)
     {
         return await _hvaKosterStrommenHourPriceService.GetHourPricesByDate(date);
+    }
+    
+    private IEnumerable<IDailyHourPrice> AdjustPricesAccordingToGovernmentSubsidies(IEnumerable<IDailyHourPrice> dailyHourPrices)
+    {
+        dailyHourPrices = dailyHourPrices.ToList();
+        foreach (var dailyHourPrice in dailyHourPrices)
+        {
+            dailyHourPrice.Price = GetAdjustedPrice(dailyHourPrice.Price, dailyHourPrice.Date);
+        }
+        return dailyHourPrices;
+    }
+
+    private decimal GetAdjustedPrice(decimal hourPrice, DateTime date)
+    {
+        if (date >= new DateTime(2024, 1, 1))
+        {
+            if (hourPrice > 0.73m)
+                return ((hourPrice - 0.73m) * 0.1m) + 0.73m;
+            
+            return hourPrice;
+        }
+        else if (date >= new DateTime(2023, 9, 1))
+        {
+            if (hourPrice > 0.70m)
+                return ((hourPrice - 0.70m) * 0.1m) + 0.70m;
+
+            return hourPrice;
+        }
+        else if (date >= new DateTime(2023, 4, 1))
+        {
+            //80% av månedlig gjennomsnitt
+            return hourPrice;
+        }
+        else if (date >= new DateTime(2022, 9, 1))
+        {
+            //90% av måndelig gjennomsnitt
+            return hourPrice;
+        }
+
+        return hourPrice;
     }
 
 }
